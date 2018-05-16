96a5777a589a5838bfa2f8d1979521fb835a2230
andre <andre.jacquemond@heig-vd.ch>
2018-05-16 20:06:08 +0200

    start ok

--- requestdispatcherthread.cpp
+++ requestdispatcherthread.cpp
@@ -5,7 +5,7 @@ void RequestDispatcherThread::run()
     while(true) {
         if (hasDebugLog)
             qDebug() << "Waiting for requests...";
-        Request req = request->get();   // block until a request is available
+        Request req = requests->get();   // block until a request is available
         if (hasDebugLog)
             qDebug() << "Got a request '" << req.getFilePath();
 
